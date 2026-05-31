305f40221f79b9d8b9c9959bdaa7308663101f41
Ayleen N Martínez G <a.martinez69@alumnos.santotomas.cl>
2026-05-30 21:19:07 -0400

    Actualizado Usuario.cs

---
+++ PruebaSegundaUnidad/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PruebaSegundaUnidad.Models
+{
+    // Datos principales de un usuario del sistema.
+    // Se usa en login, sesión, rol y después en perfil.
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public int RolId { get; set; }
+
+        public string NombreRol { get; set; }
+
+        public string NombreCompleto { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Correo { get; set; }
+
+        public string ClaveHash { get; set; }
+
+        public bool Estado { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+    }
+}
